be4db261448c0a88519cf37c8fba0d773d071fea
Milan Pála <milan.pala@peckadesign.cz>
2020-04-04 23:14:46 +0200

    Sniff hlídá počet nových řádků mezi metodami tříd

---
+++ tests/Failure/Classes/MethodSpacingSniff.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace PdTests\Failure\Classes;
+
+class MethodSpacingSniff
+{
+
+	public function hello(): void
+	{
+	}
+
+	public function hello2(): void
+	{
+	}
+
+
+
+	public function hello3(): void
+	{
+	}
+
+}
---
+++ tests/Success/Classes/MethodSpacingSniff.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+namespace PdTests\Success\Classes;
+
+class MethodSpacingSniff
+{
+
+	public function hello(): void
+	{
+	}
+
+
+	public function hello2(): void
+	{
+	}
+
+}
